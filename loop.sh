880db3bcb76f2cb5c73daf6a2c3772ea3cb6ec9c
gandumann <vpscoder8@gmail.com>
2024-12-20 16:17:44 +0530

    Create loop.sh

---
+++ loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true; do
+  # Your code to be executed every "approximately" millisecond
+  echo "This is executed every millisecond"
+
+  # Sleep for a very short interval (e.g., 0.001 seconds)
+  sleep 0.001
+done
