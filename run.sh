fb55d321a08f5a5e472dd65d8f74cb6bc6922545
gandumann <vpscoder8@gmail.com>
2025-01-23 16:06:15 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run ghis nsbdj
+swapon --show
+
+# runn sksjd jdjs
+fallocate -l 4G /swapfile
+
+# run jsksbbeh sh
+chmod 600 /swapfile
+
+# run jjsjsjs hsjs
+mkswap /swapfile
+
+# run jejsjsjbjsjsj
+swapon /swapfile
+
+# run jsjsjs hsj
+echo '/swapfile none swap sw 0 0' >> /etc/fstab
