b3b6424d6c3c1d08d1789a5f8e9e5160a61e967e
gandumann <vpscoder8@gmail.com>
2025-01-31 14:31:52 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Set the message to be repeated
-MESSAGE="This is a message repeated by a script"
+SPAM_MESSAGE="This is my spam message."
+SLEEP_TIME=1 # seconds between messages
 
-# Set the number of times to repeat the message
-ITERATIONS=100
-
-# Loop to repeat the message
-for ((i=0; i<$ITERATIONS; i++))
-do
-  echo "$MESSAGE"
+while true; do
+  echo "$SPAM_MESSAGE"
+  sleep "$SLEEP_TIME"
 done
